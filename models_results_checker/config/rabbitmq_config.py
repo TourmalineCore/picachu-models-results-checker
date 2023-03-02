708f206a4844d7c792e7622101cda3b8e34ac42e
Maria Yadryshnikova <93837471+myadryshnikova@users.noreply.github.com>
2023-03-02 11:53:26 +0500

    fix(config): split config into several files

--- models_results_checker/config/rabbitmq_config.py
+++ models_results_checker/config/rabbitmq_config.py
@@ -1,45 +1,20 @@
-"""Сonfiguration
-Use env var to override
-"""
 import os
-
-ENV = os.getenv('FLASK_ENV')
-DEBUG = ENV == 'development'
 
 rabbitmq_host = os.getenv('RABBITMQ_HOST')
 rabbitmq_username = os.getenv('RABBITMQ_DEFAULT_USER')
 rabbitmq_password = os.getenv('RABBITMQ_DEFAULT_PASS')
-
 rabbitmq_association_queue_name = os.getenv('RABBITMQ_ASSOCIATION_QUEUE_NAME')
-models_queues_dlx = os.getenv('MODELS_QUEUES_DLX')
-
-results_host = os.getenv('RESULTS_HOST')
-
+rabbitmq_models_queues_dlx = os.getenv('RABBITMQ_MODELS_QUEUES_DLX')
 
 if not rabbitmq_host:
     raise ValueError('You should specify RABBITMQ_HOST to be able to connect to RabbitMQ.')
-
 if not rabbitmq_username:
     raise ValueError('You should specify RABBITMQ_DEFAULT_USER to be able to connect to RabbitMQ.')
-
 if not rabbitmq_password:
     raise ValueError('You should specify RABBITMQ_DEFAULT_PASS to be able to connect to RabbitMQ.')
 
 if not rabbitmq_association_queue_name:
     raise ValueError('You should specify RABBITMQ_ASSOCIATION_QUEUE_NAME to be able to connect to queue with requests.')
 
-if not models_queues_dlx:
-    raise ValueError('You should specify MODELS_QUEUES_DLX to be able to connect to queue with requests.')
-
-if not results_host:
-    raise ValueError('You should specify RESULTS_HOST to be able to connect to host with models results.')
-
-
-class RabbitMQConfigProvider:
-    @staticmethod
-    def get_config():
-        return (
-            rabbitmq_host,
-            rabbitmq_username,
-            rabbitmq_password,
-        )
+if not rabbitmq_models_queues_dlx:
+    raise ValueError('You should specify RABBITMQ_MODELS_QUEUES_DLX to be able to connect to queue with requests.')
